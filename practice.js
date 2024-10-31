922cd88f55751a5bc727881a2d73edb5b65abb4f
John Park <176120986+jprk8@users.noreply.github.com>
2024-10-30 23:41:05 -0700

    Finish testing practice project

--- practice.js
+++ practice.js
@@ -1,5 +1,61 @@
-export { sum };
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
 
-function sum(a, b) {
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+function calculator() {
+  this.add = function (a, b) {
     return a + b;
+  };
+
+  this.subtract = function (a, b) {
+    return a - b;
+  };
+
+  this.divide = function (a, b) {
+    return a / b;
+  };
+
+  this.multiply = function (a, b) {
+    return a * b;
+  };
+}
+
+function caesarCipher(str, mod) {
+  return str
+    .split('')
+    .map((char) => shiftCharacter(char, mod))
+    .join('');
+}
+
+// helper function for caesarCipher
+function shiftCharacter(char, mod) {
+  let code = char.charCodeAt();
+  const punctuations = `.,?!'"-_ `;
+  if (!punctuations.includes(char)) {
+    code += mod;
+    if (char === char.toUpperCase() && code > 90) {
+      const temp = code - 90;
+      code = temp + 64;
+    }
+    if (char === char.toLowerCase() && code > 122) {
+      const temp = code - 122;
+      code = temp + 96;
+    }
+  }
+  return String.fromCharCode(code);
+}
+
+function analyzeArray(arr) {
+  return {
+    average: arr.reduce((sum, item) => sum + item) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
+  };
 }
--- practice.test.js
+++ practice.test.js
@@ -1,5 +1,71 @@
-import { sum } from './practice.js';
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from './practice.js';
 
-test('adds 1 + 2 equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('capitalize: hello to Hello', () => {
+  expect(capitalize('hello')).toBe('Hello');
+});
+
+test('reverseString: world to dlrow', () => {
+  expect(reverseString('world')).toBe('dlrow');
+});
+
+test('calculator has addition: 2 + 3 = 5', () => {
+  expect(new calculator().add(2, 3)).toBe(5);
+});
+
+test('calculator has subtract: 9 - 2 = 7', () => {
+  expect(new calculator().subtract(9, 2)).toBe(7);
+});
+
+test('calculator has divide: 10 / 2 = 5', () => {
+  expect(new calculator().divide(10, 2)).toBe(5);
+});
+
+test('calculator has multiply: 8 * 8 = 64', () => {
+  expect(new calculator().multiply(8, 8)).toBe(64);
+});
+
+test('caesarCipher works minimally: [abc] to [cde]', () => {
+  expect(caesarCipher('abc', 2)).toBe('cde');
+});
+
+test('caesarCipher works: [xyz] to [abc]', () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('caesarCipher loops with Upper Case: [XYZ] to [ABC]', () => {
+  expect(caesarCipher('XYZ', 3)).toBe('ABC');
+});
+
+test('caesarCipher preserves case: [HeLLo] to [KhOOr]', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+});
+
+test('caesarCipher preserves punctuation: [Hello, World!] to [Khoor, Zruog!]', () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
+
+test('analzyeArray returns average', () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object.average).toBe(4);
+});
+
+test('analzyeArray returns min', () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object.min).toBe(1);
+});
+
+test('analzyeArray returns max', () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object.max).toBe(8);
+});
+
+test('analzyeArray returns length', () => {
+  const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+  expect(object.length).toBe(6);
 });
